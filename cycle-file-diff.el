5fe83a76abfcd0e227e3b826a0f878b92aa41402
jack <jacktasia@gmail.com>
2016-08-12 18:09:28 -0700

    package-ify

--- cycle-file-diff.el
+++ cycle-file-diff.el
@@ -1,10 +1,37 @@
+;;; cycle-file-diff.el --- cycle through a file's changes since its last commit.
+
+;; Copyright (C) 2015-2016 jack angers
+;; Author: jack angers
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3") (s "1.11.0") (dash "2.9.0"))
+;; Keywords: programming
+
+;; Cycle File Diff is free software; you can redistribute it and/or modify it
+;; under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; Cycle File Diff is distributed in the hope that it will be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+;; or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+;; License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with Cycle File Diff.  If not, see http://www.gnu.org/licenses.
+
+;;; Commentary:
+
+;; Cycle File Diff makes it easy to move to different chunks of code that you've edited
+;; since the last time you committed the file.
+
+;;; Code:
 (require 's)
 (require 'dash)
 
 (defvar cfd-git-cmd "git --no-pager -c diff.autorefreshindex=0 diff --no-color --no-ext-diff --relative -U0 %s")
 
 (defun cfd-get-file-diff-line-starts ()
-  (interactive)
+  "Get the line numbers of all the edited chunks for the current file."
   (let* ((cmd (format cfd-git-cmd (buffer-file-name)))
          (cur-line (line-number-at-pos))
          (rawresults (shell-command-to-string cmd))
@@ -15,10 +42,11 @@
 
 
 (defun cfd-next ()
+  "Go to do the next edited chunk of code."
   (interactive)
   (let* ((lines (cfd-get-file-diff-line-starts))
-        (cur-line (line-number-at-pos))
-        (usable-lines (--filter (< cur-line it) lines)))
+         (cur-line (line-number-at-pos))
+         (usable-lines (--filter (< cur-line it) lines)))
     (if lines
       (if (> (length usable-lines) 0)
           (goto-line (car usable-lines))
@@ -26,10 +54,11 @@
       (message "%s" "Nowhere to cycle to."))))
 
 (defun cfd-prev ()
+  "Go to do the previous edited chunk of code."
   (interactive)
   (let* ((lines (cfd-get-file-diff-line-starts))
-        (cur-line (line-number-at-pos))
-        (usable-lines (--filter (> cur-line it) lines)))
+         (cur-line (line-number-at-pos))
+         (usable-lines (--filter (> cur-line it) lines)))
     (if lines
       (if (> (length usable-lines) 0)
           (goto-line (car (last usable-lines)))
@@ -38,3 +67,6 @@
 
 (global-set-key (kbd "C-c 8") 'cfd-prev)
 (global-set-key (kbd "C-c 9") 'cfd-next)
+
+(provide 'cycle-file-diff)
+;;; cycle-file-diff.el ends here
